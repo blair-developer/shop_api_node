cad33d8094078d8a1a7b40cc53246ee8e8d84005
blair-developer <blairdeveloper17@gmail.com>
2024-06-06 14:15:58 +0300

    ap.js

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const Shopify = require('shopify-api-node')
+
+const shopify = new Shopify({
+    shopName: process.env.SHOPIFY_STORE_URL,
+    apiKey: process.env.SHOPIFY_API_KEY,
+    password: process.env.SHOPIFY_ACCESS_TOKEN
+})
+
+async function fetchProducts(){
+    try {
+        const products = await shopify.product.list()
+        console.log(products)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+fetchProducts()
